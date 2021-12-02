3e4e0d341cf81d7bbf03df9611ce9f33c7d29308
wissal bensadoun <wissal.bensadoun@hotmail.fr>
2021-12-02 19:06:08 +0100

    formulaire voiture

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Car;
+use App\Form\CarType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/voitures")
+ */
 class CarController extends AbstractController
 {
-    #[Route('/car', name: 'car')]
-    public function index(): Response
-    {
-        return $this->render('car/index.html.twig', [
-            'controller_name' => 'CarController',
+    /**
+     * @Route("/creer",name="create_car")
+     */
+    public function create(){
+        $car = new Car(); //Nouvelle objet car
+
+        $form = $this->createForm(CarType::class, $car); //createForm appartient a AbsatractController
+        //$form = instance de formulaire dériver du modèle carType
+
+        return $this->render("car/index.html.twig", [
+            "form" => $form->createView()
         ]);
     }
+
 }
---
+++ src/Form/CarType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Car;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CarType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class,[
+                "required" => true,
+                "label" => "Nom de la voiture"
+
+            ])
+            ->add('description')
+            ->add('startDate')
+            ->add('model')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Car::class,
+        ]);
+    }
+}
