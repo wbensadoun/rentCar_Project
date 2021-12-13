502fbcea017f5cd9e6ea00fb5dd93be1f235c006
wissal <wissal.bensadoun@hotmail.fr>
2021-12-13 18:10:25 +0100

    création formulaire customer

---
+++ migrations/Version20211213142231.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211213142231 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE car ADD number_plate VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE customer ADD city VARCHAR(255) NOT NULL, ADD postal_code VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE car DROP number_plate');
+        $this->addSql('ALTER TABLE customer DROP city, DROP postal_code');
+    }
+}
