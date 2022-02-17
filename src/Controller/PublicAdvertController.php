d26e4128f36118011f4c7317d28dff976c86410e
BODA Ludovic <contact@bldeveloppement.fr>
2022-02-17 13:08:07 +0100

    ajout maildev

--- src/Controller/PublicAdvertController.php
+++ src/Controller/PublicAdvertController.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Controller;
-
+use Symfony\Component\Mime\Email;
 use App\Entity\Advert;
 use App\Repository\AdvertRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 /**
@@ -23,5 +24,27 @@ class PublicAdvertController extends AbstractController
       return $this->render("public_advert/index.html.twig",[
           'public_adverts'=> $publicAdverts
       ]);
+    }
+
+    /**
+     * @Route("/test-mail")
+     * @param MailerInterface $mailer
+     * @return void
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     */
+    public function testMail(MailerInterface $mailer)
+    {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
     }
 }
